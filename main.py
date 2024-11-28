ff3ef70969a1fe609f201b1ed287b234d50a0526
Alexandre Sauvaget <70508923+a-sauvaget@users.noreply.github.com>
2024-11-28 17:35:56 +0100

    add asteroids splitting + rate limit to player shot

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
+import sys
 import pygame
 
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
 	pygame.init()
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
 	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
 
 	Player.containers = (updatable, drawable)
+	Shot.containers = (shots, updatable, drawable)
 	Asteroid.containers = (asteroids, updatable, drawable)
 	AsteroidField.containers = (updatable)
+
 
 	asteroid_field = AsteroidField()
 	player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2, radius = PLAYER_RADIUS)
@@ -36,11 +41,17 @@ def main():
 		
 		for obj in updatable:
 			obj.update(dt)
-		
-		for obj in asteroids:
-			if obj.collides_with(player) == True:
+
+		for asteroid in asteroids:
+			if asteroid.collides_with(player) == True:
 				print("Game over !")
 				sys.exit()
+
+			for shot in shots:
+				if shot.collides_with(asteroid):
+					asteroid.split()
+					shot.kill()
+			
 
 		pygame.display.flip()
 
