4d5e7adde4919e050c9398282f7071b264995c36
Alexandre Sauvaget <70508923+a-sauvaget@users.noreply.github.com>
2024-11-28 14:22:11 +0100

    add game constants

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
 
+from constants import *
+
 def main():
 	print("Starting asteroids!")
+	print(SCREEN_WIDTH)
+	print(SCREEN_HEIGHT)
 
 if __name__ == "__main__":
-    main()
+	main()
