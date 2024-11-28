2d80b26701203997d87d5c6fea7c73d61d6fabe0
Alexandre Sauvaget <70508923+a-sauvaget@users.noreply.github.com>
2024-11-28 14:59:53 +0100

    add clock to limit FPS at 60

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from constants import *
 
 def main():
 	pygame.init()
-	print("Starting asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0
+	
 	# Game loop
 	while True:
 		# This will check if the user has closed the window and exit the game loop if they do. 
@@ -16,8 +16,9 @@ def main():
 			if event.type == pygame.QUIT:
 				return
 
-		screen.fill((0, 0, 0))
+		screen.fill("black")
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
