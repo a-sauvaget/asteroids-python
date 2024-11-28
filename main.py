660a27dd4e3046fe075dd4b6e90df4110aaedc49
Alexandre Sauvaget <70508923+a-sauvaget@users.noreply.github.com>
2024-11-28 15:25:32 +0100

    add Player initial state and shape

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
 	pygame.init()
+	player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2, radius = PLAYER_RADIUS)
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
 	dt = 0
@@ -17,7 +19,12 @@ def main():
 				return
 
 		screen.fill("black")
+
+		player.draw(screen)
+
 		pygame.display.flip()
+
+		# limit the framerate to 60 FPS
 		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
