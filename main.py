9e4203ef8cc1dbb3e5e6bcaa4651df87cd610e8a
Alexandre Sauvaget <70508923+a-sauvaget@users.noreply.github.com>
2024-11-28 14:44:48 +0100

    add gitignore + init game screen

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ import pygame
 from constants import *
 
 def main():
+	pygame.init()
 	print("Starting asteroids!")
-	print(SCREEN_WIDTH)
-	print(SCREEN_HEIGHT)
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	# Game loop
+	while True:
+		# This will check if the user has closed the window and exit the game loop if they do. 
+		# It will make the window's close button work.
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+
+		screen.fill((0, 0, 0))
+		pygame.display.flip()
 
 if __name__ == "__main__":
 	main()
